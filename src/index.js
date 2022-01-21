6ff843869c1b294a56a20e50a762abd031920e21
Kuntash <kuntashtashi11@gmail.com>
2022-01-21 14:59:23 +0530

    Changed README.md, added theme for material UI, work on LOGIN page starts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+// TODO: Change the links in the readme
+// TODO: Upload on a public git repository
+// TODO: Host the website on google firestore or netlify
+// TODO: Take Screenshots of the website
+// TODO: Screen Record of the deployed website and its working
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/system";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#fa2611"
+        },
+        secondary: {
+            main: "rgba(0,0,0,0.16)"
+        }
+    }
+});
+
+export default theme;
